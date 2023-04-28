1a2afeb82ea9fe165e9d49264cd4d01743c36ef5
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-04-28 19:00:36 -0300

    class number 12 has ended

--- routes/products.router.js
+++ routes/products.router.js
@@ -15,12 +15,26 @@ router.get('/', (req, res) => {
       image: faker.image.imageUrl(),
     });
   }
-  res.json(products);
+  res.status(200).json(products);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({ message: 'Not Found' });
+  } else {
+    res.status(200).json({
+      id,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body,
   });
@@ -29,7 +43,7 @@ router.post('/', (req, res) => {
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
+  res.status(204).json({
     message: 'updated',
     data: body,
     id,
@@ -38,7 +52,7 @@ router.patch('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
+  res.status(204).json({
     message: 'deleted',
     id,
   });
