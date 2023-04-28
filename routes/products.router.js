8764d63e05ed5f1b93d9d57a725d613944accb96
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-04-28 18:52:16 -0300

    class number 11 has ended

--- routes/products.router.js
+++ routes/products.router.js
@@ -18,4 +18,30 @@ router.get('/', (req, res) => {
   res.json(products);
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router;
