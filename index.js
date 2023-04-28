6465a518600e6372c7912e931d4d0d16f5090601
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-04-28 18:08:56 -0300

    class number 7 has ended

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      id: faker.datatype.uuid(),
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.listen(port, () => {
