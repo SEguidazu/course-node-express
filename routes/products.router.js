99d1f366c352de22511bfef63af94167e21b74ea
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-04-28 19:29:07 -0300

    class number 15 has ended

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,61 +1,41 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/product.services');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      id: faker.datatype.uuid(),
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.status(200).json(products);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({ message: 'Not Found' });
+  const product = service.findOne(id);
+  if (product) {
+    res.status(200).json(product);
   } else {
-    res.status(200).json({
-      id,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
+    res.status(404).json({ message: 'Product not found' });
   }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body,
-  });
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
 });
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.status(204).json({
-    message: 'updated',
-    data: body,
-    id,
-  });
+  const product = service.update(id, body);
+  res.status(204).json(product);
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.status(204).json({
-    message: 'deleted',
-    id,
-  });
+  res.status(204).json(service.delete(id));
 });
 
 module.exports = router;
