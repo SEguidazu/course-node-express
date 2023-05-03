270a45d80dad547e96c0045a67a88c24d383d11b
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-05-03 20:38:45 -0300

    fix migration issue with schemas

--- db/migrations/20230501010734-create-user.js
+++ db/migrations/20230501010734-create-user.js
@@ -1,11 +1,40 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { USER_TABLE } = require('../models/user.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'customer',
+      },
+      createAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
   async down(queryInterface) {
